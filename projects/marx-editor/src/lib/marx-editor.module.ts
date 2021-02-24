8af5980eaa31525139da7877bb8cf7b846b50cc3
Alec Aldrine Lakra <lakraalec123@gmail.com>
2021-02-24 16:31:25 +0530

    package details

--- projects/marx-editor/src/lib/marx-editor.module.ts
+++ projects/marx-editor/src/lib/marx-editor.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { MarxEditorComponent } from './marx-editor.component';
 import { EditorMenuComponent } from '../editor-menu/editor-menu.component';
 import { ToolsModule } from '../tools/tools.module';
-
+import { MentionModule } from 'angular-mentions';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [MarxEditorComponent, EditorMenuComponent],
   imports: [
-    ToolsModule
+    ToolsModule,
+    MentionModule,
+    FormsModule
   ],
   exports: [MarxEditorComponent]
 })
