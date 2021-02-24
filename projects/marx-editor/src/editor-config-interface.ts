e5d5702e76bd4d39e05b7a375a38e70a5655b90c
Alec Aldrine Lakra <lakraalec123@gmail.com>
2021-02-24 15:58:03 +0530

    changes editor

---
+++ projects/marx-editor/src/editor-config-interface.ts
@@ -0,0 +1,31 @@
+export interface EditorConfig {
+    file?: boolean;
+    mentionedNames?: MentionedName[];
+    mentionedDates?: string[];
+    colorPalette?: boolean;
+    buttonName?: string;
+    toolbarPlacement?: 'top' | 'bottom';
+    placeholder?: string;
+    mode?: 'basic' | 'prime'
+}
+
+export interface MentionedName {
+    id: number;
+    name: string;
+}
+
+export interface ToolbarConfig {
+    bold?: boolean;
+    italic?: boolean;
+    strikeThrough?: boolean;
+    underline?: boolean;
+    orderedList?: boolean;
+    unorderedList?: boolean;
+    superscript?: boolean;
+    subscript?: boolean;
+    quote?: boolean;
+    fontStyle?: string,
+    fontColor: string;
+    backgroundColor: string;
+}
+
--- projects/marx-editor/src/lib/marx-editor.module.ts
+++ projects/marx-editor/src/lib/marx-editor.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MarxEditorComponent } from './marx-editor.component';
-
-
+import { EditorMenuComponent } from '../editor-menu/editor-menu.component';
+import { ToolsModule } from '../tools/tools.module';
 
 @NgModule({
-  declarations: [MarxEditorComponent],
+  declarations: [MarxEditorComponent, EditorMenuComponent],
   imports: [
+    ToolsModule
   ],
   exports: [MarxEditorComponent]
 })
