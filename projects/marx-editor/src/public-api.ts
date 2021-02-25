25812ffe8491fe7e14180fbe8464c6c8f780e720
Alec Aldrine Lakra <alec@excellencetech.com>
2021-02-25 12:30:08 +0530

    exporting editor module

--- projects/marx-editor/src/public-api.ts
+++ projects/marx-editor/src/public-api.ts
@@ -3,6 +3,8 @@
  * Public API Surface of marx-editor
 */
 
+import { MarxEditorModule } from "marx-editor";
+
 // export * from './lib/marx-editor.service';
 // export * from './lib/marx-editor.component';
-export * from './editor-container/marx-editor.module';
+export default MarxEditorModule;
